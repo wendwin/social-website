7890cb733d3cad624c96c0cbdaeb165405eff218
wendwin <wendwin12@gmail.com>
2025-02-10 21:03:06 +0700

    menambahkan autentikasi views pada login dan logout view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,23 +3,32 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+from django.contrib.auth.decorators import login_required
 
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'], password=cd['password'])
 
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authenticated successfully')
+#                 else:
+#                     return HttpResponse('Disabled account')
+#             else:
+#                 return HttpResponse('Invalid login')
 
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
