e12c988a12757356294c4405c4ca9437388092be
wendwin <wendwin12@gmail.com>
2025-02-12 01:34:41 +0700

    membuat fitur user registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 from django.contrib.auth.decorators import login_required
 
@@ -31,4 +31,30 @@ def dashboard(request):
         request,
         'account/dashboard.html',
         {'section': 'dashboard'}
+    )
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+
+            new_user.save()
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
     )
