f961504ae80f91d9f80b46e1f439a79af671d357
wendwin <wendwin12@gmail.com>
2025-02-13 20:58:23 +0700

    menambahkan custom authentication backend

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -144,3 +144,8 @@ PASSWORD_HASHERS = [
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+'account.authentication.EmailAuthBackend',
+]
